d1ed5e5ba132aece35a625bc1b5a4fcef5536f3b
Jinhao Huang <me@jinhaohuang.com>
2025-04-16 11:23:24 +0800

    refactor: rename config variables and improve pre-check logic

--- crash_locator/config.py
+++ crash_locator/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 class Config:
-    CRASH_REPORT_PATH: Path = Path.cwd() / "Data" / "crash_reports" / "all-0328"
-    PRE_CHECK_PATH: Path = Path.cwd() / "Data" / "TSE25" / "pre_check"
+    CRASH_REPORTS_DIR: Path = Path.cwd() / "Data" / "crash_reports" / "all-0328"
+    PRE_CHECK_REPORTS_DIR: Path = Path.cwd() / "Data" / "TSE25" / "pre_check"
--- crash_locator/pre_check.py
+++ crash_locator/pre_check.py
@@ -2,13 +2,14 @@ import logging
 from .config import Config
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
+import shutil
 
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    crash_reports_path = Config.CRASH_REPORT_PATH
-    pre_check_path = Config.PRE_CHECK_PATH
-    work_list = crash_reports_path.iterdir()
+    crash_reports_dir = Config.CRASH_REPORTS_DIR
+    pre_check_reports_dir = Config.PRE_CHECK_REPORTS_DIR
+    work_list = crash_reports_dir.iterdir()
     
     with logging_redirect_tqdm():
         for crash_report_dir in tqdm(list(work_list)):
@@ -17,5 +18,9 @@ if __name__ == "__main__":
             if not crash_report_path.exists():
                 logger.error(f"Crash report {report_name} not found")
                 continue
+
+            pre_check_report_path = pre_check_reports_dir / report_name
+            pre_check_report_path.mkdir(parents=True, exist_ok=True)
+            shutil.copy(crash_report_path, pre_check_report_path)
             
             logger.info(f"Crash report {report_name} found")
