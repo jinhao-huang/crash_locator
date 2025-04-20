b8fd96fac4312d6591c114e76fd8aa9cb6abecc7
Jinhao Huang <me@jinhaohuang.com>
2025-04-20 21:34:52 +0800

    feat: update candidate method response guidelines to minimize "Yes" replies

--- crash_locator/prompt.py
+++ crash_locator/prompt.py
@@ -10,7 +10,7 @@ You will be given a crash report first, then you need to analyze the crash repor
 
 Then, we will give you a candidate method at a time, and you need to analyze whether the candidate method is related to the crash.
 
-If this candidate method is related to the crash, you just reply "Yes", otherwise you reply "No" without any additional text.
+For those candidate methods that are most likely to be related to the crash, you just reply "Yes"(Usually the numbers "Yes" is less than 3, so reply "Yes" as few as possible), otherwise you reply "No" without any additional text.
 """
 
     @staticmethod
