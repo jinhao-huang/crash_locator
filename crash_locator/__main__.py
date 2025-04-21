1893fbcd4c24dcb2b481abbb6d7959f1a10d9156
Jinhao Huang <me@jinhaohuang.com>
2025-04-21 14:20:56 +0800

    refactor: __main__.py structure

--- crash_locator/__main__.py
+++ crash_locator/__main__.py
@@ -1,13 +1,12 @@
 import shutil
+from pathlib import Path
 from crash_locator.config import Config, setup_logging
-from crash_locator.utils.java_parser import get_application_code
 from crash_locator.my_types import (
     ReportInfo,
     RunStatistic,
     Candidate,
     FinishedReportInfo,
     SkippedReportInfo,
-    TemporarySkippedReportInfo,
 )
 from crash_locator.utils.llm import query_filter_candidate
 from tqdm import tqdm
@@ -16,16 +15,6 @@ import logging
 import json
 
 logger = logging.getLogger(__name__)
-
-
-def llm_filter(report_info: ReportInfo) -> list[Candidate]:
-    # Check all candidates before querying LLM
-    for candidate in report_info.candidates:
-        get_application_code(report_info.apk_name, candidate.signature)
-        candidate.reasons.reason_explanation()
-
-    # Query LLM
-    return query_filter_candidate(report_info)
 
 
 # Check if the buggy method is filtered
@@ -38,7 +27,7 @@ def _is_buggy_method_filtered(
     return True
 
 
-def filter_statistic(
+def _statistic(
     report_info: ReportInfo,
     remaining_candidates: list[Candidate],
     statistic: RunStatistic,
@@ -52,36 +41,57 @@ def filter_statistic(
     if _is_buggy_method_filtered(report_info, remaining_candidates):
         statistic.filtered_buggy_method += 1
 
+    statistic.finished_reports[report_info.apk_name] = FinishedReportInfo(
+        total_candidates_count=len(report_info.candidates),
+        remaining_candidates_count=len(remaining_candidates),
+        is_buggy_method_filtered=_is_buggy_method_filtered(
+            report_info, remaining_candidates
+        ),
+    )
 
-def save_statistic(statistic: RunStatistic):
+
+def _save_statistic(statistic: RunStatistic):
     with open(Config.RESULT_STATISTIC_PATH, "w") as f:
         f.write(statistic.model_dump_json(indent=4))
 
 
-if __name__ == "__main__":
-    setup_logging(Config.RESULT_LOG_FILE_PATH)
-
+def _get_statistic() -> RunStatistic:
     if Config.RESULT_STATISTIC_PATH.exists():
+        logger.info(f"Load statistic from {Config.RESULT_STATISTIC_PATH}")
         with open(Config.RESULT_STATISTIC_PATH, "r") as f:
             statistic = RunStatistic(**json.load(f))
     else:
+        logger.info("No statistic file found, create a new one")
         statistic = RunStatistic()
+    return statistic
 
+
+def _get_work_list() -> list[Path]:
     if Config.DEBUG:
-        work_list = [Config.DEBUG_PRE_CHECK_REPORT_DIR]
+        logger.info(f"Use debug mode, only process {Config.DEBUG_PRE_CHECK_REPORT_DIR}")
+        return [Config.DEBUG_PRE_CHECK_REPORT_DIR]
     else:
-        work_list = Config.PRE_CHECK_REPORTS_DIR.iterdir()
+        logger.info(f"Process all reports in {Config.PRE_CHECK_REPORTS_DIR}")
+        return list(Config.PRE_CHECK_REPORTS_DIR.iterdir())
+
+
+def main():
+    setup_logging(Config.RESULT_LOG_FILE_PATH)
+    logger.info("Start processing reports")
+
+    statistic = _get_statistic()
+    work_list = _get_work_list()
 
     with logging_redirect_tqdm():
         for pre_check_report_dir in tqdm(list(work_list), desc="Processing reports"):
-            save_statistic(statistic)
+            _save_statistic(statistic)
 
             logger.info(f"Processing report {pre_check_report_dir.name}")
             logger.debug(f"Report path: {pre_check_report_dir}")
 
             report_name = pre_check_report_dir.name
             if report_name in statistic.finished_reports:
-                logger.info(f"Report {report_name} already processed")
+                logger.info(f"Report {report_name} already processed, skip it")
                 continue
 
             report_path = pre_check_report_dir / Config.PRE_CHECK_REPORT_INFO_NAME
@@ -89,21 +99,23 @@ if __name__ == "__main__":
                 report_info = ReportInfo(**json.load(f))
 
             if len(report_info.candidates) == 1:
+                logger.info(f"Report {report_name} has only one candidate, skip it")
                 statistic.finished_reports[report_name] = SkippedReportInfo()
                 continue
 
+            logger.info(
+                f"Copy report {report_name} to {Config.RESULT_REPORT_DIR(report_name)}"
+            )
             shutil.copytree(
                 pre_check_report_dir,
                 Config.RESULT_REPORT_DIR(report_name),
             )
-            remaining_candidates = llm_filter(report_info)
-            filter_statistic(report_info, remaining_candidates, statistic)
-            statistic.finished_reports[report_name] = FinishedReportInfo(
-                total_candidates_count=len(report_info.candidates),
-                remaining_candidates_count=len(remaining_candidates),
-                is_buggy_method_filtered=_is_buggy_method_filtered(
-                    report_info, remaining_candidates
-                ),
-            )
+
+            remaining_candidates = query_filter_candidate(report_info)
+            _statistic(report_info, remaining_candidates, statistic)
 
     logger.info(f"Statistic: {statistic}")
+
+
+if __name__ == "__main__":
+    main()
